711655a79aec4d0dcc8cbb3b449d47cbbf882b45
Korex23 <ololokes@gmail.com>
2023-08-10 00:45:03 -0700

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - entry point
+ * main - Entry point
  *
  * Description : prints a text using the printf function
  * Return : Always 0 (Success)
@@ -9,7 +9,7 @@
 int main(void)
 {
 	/*use printf to print the text*/
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	/*Return 0 on successfull completion*/
-	return(0);
+	return (0);
 }
