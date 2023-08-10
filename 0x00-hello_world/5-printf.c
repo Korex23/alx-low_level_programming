960d5e761e8e251addd356b4e2cf40ad8821420f
Korex23 <ololokes@gmail.com>
2023-08-10 00:40:19 -0700

    second c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Description : prints a text using the printf function
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	/*use printf to print the text*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+	/*Return 0 on successfull completion*/
+	return(0);
+}
