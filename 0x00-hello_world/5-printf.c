75019f57e2e646632c6420141b061a31ba869423
Korex23 <ololokes@gmail.com>
2023-08-10 00:48:30 -0700

    fuck betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	/*use printf to print the text*/
+	/* Use printf to print the text */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/*Return 0 on successfull completion*/
+	/* Return 0 on successfull completion */
 	return (0);
 }
