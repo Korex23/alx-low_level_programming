8dd9d9dc8bf65ae1b8374d39ecf86037ed0c378d
Korex23 <ololokes@gmail.com>
2023-08-11 15:26:13 -0700

    advanced one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints out every possible 2 digit combination
+ * Return: Always 0
+ */
+int main(void)
+{
+	int p, q;
+
+
+	for (p = 0; p <= 8; p++)
+	{
+		for (q = p + 1; q <= 9; q++)
+		{
+			putchar(p + '0')
+			putchar(q + '0');
+			if (p == 8 && q == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
