16ef8df7e1d4a5599d31db0a5805a3f775132623
Korex23 <ololokes@gmail.com>
2023-08-16 00:55:56 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 3;
+	char c;
+
+	while (i <= 9)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
