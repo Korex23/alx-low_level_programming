f9648a8907d39295bd0998f2e6ba95cad596e9f6
Korex23 <ololokes@gmail.com>
2023-08-16 02:04:18 -0700

    7th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @n: Charachter to be absolute
+ * Return: Depending on the value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
