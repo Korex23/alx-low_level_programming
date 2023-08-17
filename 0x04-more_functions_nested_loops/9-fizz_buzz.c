9cbdbc4b2bbd2188ac99fb0bf07d1c9f2d7d1f78
Korex23 <ololokes@gmail.com>
2023-08-17 06:32:58 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			_printf(" ");
+	}
+	putchar('\n');
+	return (0);
+}
