661c0c6c81f9a0869ca61921cffe079e3cbafec3
Korex23 <ololokes@gmail.com>
2023-08-22 23:57:05 -0700

    5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a strign in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s);
+{
+	char *ptr = s + _strlen(s) - 1;
+
+	while (ptr >= s)
+	{
+		putchar(*ptr);
+		ptr--;
+	}
+	putchar('\n');
+}
