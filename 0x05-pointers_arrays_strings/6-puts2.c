31f985770e83241d71326722e40c271e90f164a9
Korex23 <ololokes@gmail.com>
2023-08-24 07:31:33 -0700

    7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints stringd
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+			putchar('\n');
+		}
+		i++;
+	}
+}
